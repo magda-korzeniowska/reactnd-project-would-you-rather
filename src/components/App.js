320e13551e74a9b8ec26a40487cfad30d4a7910d
magda-korzeniowska <m.korzeniowska@tlen.pl>
2019-04-01 17:25:05 +0200

    Add rendering Dashboard only when loading initial data is finished

--- src/components/App.js
+++ src/components/App.js
@@ -12,10 +12,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Dashboard />
+        {this.props.loading === true
+          ? null
+          : <Dashboard />}      
       </div>
     );
   }
 }
 
-export default connect()(App);
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App);
