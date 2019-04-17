613284f8ea6ffc55ebcd396d50dbd59053a97e15
magda-korzeniowska <m.korzeniowska@tlen.pl>
2019-04-17 16:51:28 +0200

    Updates

---
+++ precache-manifest.acabe0188f988cc3be92b383687692ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d69048ddefef80bc082f0837b09496",
+    "url": "/reactnd-project-would-you-rather/static/media/vipin-cordoba.d9d69048.jpg"
+  },
+  {
+    "revision": "76547045521763fc03342a603ddd29af",
+    "url": "/reactnd-project-would-you-rather/static/media/erik-roman.76547045.jpg"
+  },
+  {
+    "revision": "bb16ee272b2359211636768596830433",
+    "url": "/reactnd-project-would-you-rather/static/media/angie-newman.bb16ee27.jpg"
+  },
+  {
+    "revision": "551335df337e630267ff",
+    "url": "/reactnd-project-would-you-rather/static/js/runtime~main.ea9e1c82.js"
+  },
+  {
+    "revision": "6f5333d3fe0052eff776",
+    "url": "/reactnd-project-would-you-rather/static/js/main.a0817317.chunk.js"
+  },
+  {
+    "revision": "ad7c2b54f00b0900656b",
+    "url": "/reactnd-project-would-you-rather/static/js/2.4fcec09f.chunk.js"
+  },
+  {
+    "revision": "6f5333d3fe0052eff776",
+    "url": "/reactnd-project-would-you-rather/static/css/main.9485ab6e.chunk.css"
+  },
+  {
+    "revision": "6427503eb66cd29aa499142dc2a22151",
+    "url": "/reactnd-project-would-you-rather/index.html"
+  }
+];
