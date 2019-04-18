f10b263ba57a23d3777650b91858133106e4901f
magda-korzeniowska <m.korzeniowska@tlen.pl>
2019-04-18 12:59:54 +0200

    Add navigation menu to PageNotFound view

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Nav from './Nav'
 
 function PageNotFound(props) {
   return (
-    <div>
+    <div class='page-not-found-container'>
+      <Nav />
       <h2 className='page-not-found'>404 Page Not Found</h2>
     </div>
   )
